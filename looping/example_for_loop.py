9fb2c508a4f648d799c1869883d283a1948831df
lulu2002 <lose9749@gmail.com>
2022-11-19 10:05:36 +0800

    feat: add for-loop practice

---
+++ looping/example_for_loop.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    for i in reversed(range(6, 10)):
+        print("hello " + str(i) + " yes.")
+
+    listA = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+
+    for i in listA:
+        print(i, end=", ")
